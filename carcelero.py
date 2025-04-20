83e4c13d980e22f94e79225568896cc8c76678c5
JaimeCM <jaimecristobalmartin@gmail.com>
2025-04-20 13:09:35 +0200

    Update carcelero.py

--- carcelero.py
+++ carcelero.py
@@ -85,7 +85,7 @@ class DesktopCarcelero:
 
     def volver_a_seleccion_usuario(self):
         self.master.destroy()
-        from menu import Aplicacion   # asegúrate de que tu primer script esté en menu.py
+        from dekstop import Aplicacion   # asegúrate de que tu primer script esté en menu.py
         root = tk.Tk()
         Aplicacion(root)
         root.mainloop()
